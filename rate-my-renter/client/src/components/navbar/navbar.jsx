96730da078c4cada606aa84e74d507d408475020
brcodington78 <brcodington@gmail.com>
2021-10-27 13:16:50 -0700

    google Oath is working

--- rate-my-renter/client/src/components/navbar/navbar.jsx
+++ rate-my-renter/client/src/components/navbar/navbar.jsx
@@ -1,23 +1,38 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useHistory, useLocation} from 'react-router-dom'
 import './stylesheets/nav.css';
 import './stylesheets/search.css';
+import { useDispatch } from 'react-redux';
+
 
 const Nav = (props) => {
 
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+
 
   console.log("user", user)
 
+  //used to set the user if the user is logged in 
   useEffect(() => {
     const token = user?.token;
-    //eventually check for JWT
 
     setUser(JSON.parse(localStorage.getItem('profile')))
-  },[])
+  },[location])
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT'});
+    history.push('/')
+
+    setUser(null)
+  }
+
+
 
   const displayRight = user ? (<div id="session-btns">
-    <button id='logout-btn' className="session-btn">Logout</button>
+    <button id='logout-btn' className="session-btn" onClick={logout}>Logout</button>
   </div>) 
   :(
     <div id="session-btns">
@@ -28,18 +43,22 @@ const Nav = (props) => {
     </div>
   )
 
+
+
+
   const searchBar = (
     <div id="search-bar">
       Search
     </div>
   )
 
+
   const displayLeft = (
     <Link to="/">
       <div>Logo</div>
     </Link>
   )
-
+  
   return (
     <div id="nav-container">
       {displayLeft}
