db83815ff3f33e2d547e247f6286f3ca269b5a69
brcodington78 <brcodington@gmail.com>
2021-10-27 10:33:28 -0700

    login form completed

--- rate-my-renter/client/src/components/Auth/Auth.js
+++ rate-my-renter/client/src/components/Auth/Auth.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserForm from '../Forms/UserForm/UserRegForm.js'
 
 
 function Auth() {
-
-    const isSignup = false;
+    let [isSignup, setIsSignup] = useState(false)
+    
     return (
         <>
           <h1>{isSignup ? "Sign Up!" : "Log in!" }</h1>
-          <UserForm />  
+          <UserForm isSignup={isSignup} setIsSignup={setIsSignup}/>  
         </>
     )
 }
--- rate-my-renter/client/src/components/Forms/UserForm/UserRegForm.js
+++ rate-my-renter/client/src/components/Forms/UserForm/UserRegForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function UserForm() {
+function UserForm({isSignup, setIsSignup}) {
   let [username, setUsername] = useState("");
   let [password, setPassword] = useState("");
   let [firstName, setFirstName] = useState("");
@@ -8,50 +8,75 @@ function UserForm() {
   let [email, setEmail] = useState("");
   let [isLandLord, setisLandLord] = useState(false)
 
-  function handleSubmit() {
-  
+  console.log("setIs", setIsSignup)
+  console.log("isSi", isSignup)
+  // useEffect(() => {
+  //   let firstNameInput = document.querySelector("#firstname-input");
+  //   firstNameInput.focus()
+  // }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
   }
-  console.log(isLandLord)
+
+  function switchMode(e) {
+    e.preventDefault()
+    setIsSignup(!isSignup)
+  }
+  
+
   return (
     <div>
-      <h1>Sign Up Here!</h1>
       <form className="user-reg-form" onSubmit={handleSubmit}>
-        <div id="user-reg-radio-btn-container">
-        <input
-          type="radio"
-          id="reg-user-radio-btn"
-          name="identity-type"
-          value="reg-user"
-          defaultChecked
-          onClick={() => setisLandLord(false)}
-        />
-        <label htmlFor="reg-user-radio-btn">I am not a landlord</label>
-        <input
-          type="radio"
-          id="landlord-radio-btn"
-          name="identity-type"
-          value="landlord"
-          onClick={() => setisLandLord(true)}
-        />
-        <label htmlFor="landlord-radio-btn">I am a landlord</label>
-        </div>
-        <br />
-        <label htmlFor="firstname-input">First Name:</label>
-        <input
-          type="text"
-          id="firstname-input"
-          placeholder="First Name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <label htmlFor="lastname-input">Last Name:</label>
-        <input
-          type="text"
-          id="lastname-input"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
+        {isSignup && (
+          <>
+            <div id="user-reg-radio-btn-container">
+              <input
+                type="radio"
+                id="reg-user-radio-btn"
+                name="identity-type"
+                value="reg-user"
+                defaultChecked
+                onClick={() => setisLandLord(false)}
+              />
+              <label htmlFor="reg-user-radio-btn">I am not a landlord</label>
+              <input
+                type="radio"
+                id="landlord-radio-btn"
+                name="identity-type"
+                value="landlord"
+                onClick={() => setisLandLord(true)}
+              />
+              <label htmlFor="landlord-radio-btn">I am a landlord</label>
+            </div>
+            <br />
+            <label htmlFor="firstname-input">First Name:</label>
+            <input
+              type="text"
+              id="firstname-input"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label htmlFor="lastname-input">Last Name:</label>
+            <input
+              type="text"
+              id="lastname-input"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <label htmlFor="username-input">Username:</label>
+            <input
+              type="text"
+              id="username-input"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </>
+        )}
+
         <label htmlFor="email-input">Email:</label>
         <input
           type="email"
@@ -59,14 +84,6 @@ function UserForm() {
           placeholder="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="username-input">Username:</label>
-        <input
-          type="text"
-          id="username-input"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
         />
         <label htmlFor="password-input">Password:</label>
         <input
@@ -76,7 +93,10 @@ function UserForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
+        <button onClick={switchMode}>
+          {isSignup ? "Already have an account? Log in!" : "Don't have an account? Sign Up!"}
+          </button>
       </form>
     </div>
   );
