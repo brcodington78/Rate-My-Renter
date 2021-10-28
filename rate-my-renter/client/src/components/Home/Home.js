2cf1e99734a34029d054e14ed9f3396ec150f302
Zach <zachsvaillancourt@gmail.com>
2021-10-27 19:39:23 -0700

    review stuff

--- rate-my-renter/client/src/components/Home/Home.js
+++ rate-my-renter/client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Reviews from '../Reviews';
 
 function Home() {
     return (
         <div>
             <h1>This is the Home Component</h1>
             <h2> We will populate it with something eventually</h2>
+            <Reviews />
         </div>
     )
 }
