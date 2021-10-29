aa18a1dbd1da7b33f9d55fbd7c5d811844875404
brcodington78 <brcodington@gmail.com>
2021-10-28 19:25:14 -0700

    minor change to auth

--- rate-my-renter/client/src/api/index.js
+++ rate-my-renter/client/src/api/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:6000'})
+const API = axios.create({ baseURL: 'http://localhost:6000'});
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
 
 
 
