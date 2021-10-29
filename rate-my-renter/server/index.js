87733052cfc7ad9310cdbc014c2244a5f8fdd144
brcodington78 <brcodington@gmail.com>
2021-10-28 17:44:28 -0700

    having port issues, changed server port from 5000 to 6000

--- rate-my-renter/server/index.js
+++ rate-my-renter/server/index.js
@@ -17,7 +17,7 @@ app.use("/reviews", reviewRoutes)
 app.use("/user", userRoutes)
 
 const CONNECTION_URL = "mongodb+srv://dev:poopybutt11@cluster0.tbzqw.mongodb.net/rateMyRenterDB?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on on port: ${PORT}`)))
